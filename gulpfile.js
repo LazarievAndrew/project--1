39dc6bae4f2dbcf137b1d28fc5046ea12cae7f35
Andrew Lazariev <LazarievAndrew@users.noreply.github.com>
2019-06-07 21:48:08 +0300

    build change

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+let cleanCSS = require('gulp-clean-css');
 
 sass.compiler = require('node-sass');
 
 gulp.task('scss', function () {
     return gulp.src('./src/scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./build/css'));
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./build/css'));
 });
 
 gulp.task('watch', function () {
     gulp.watch('./src/scss/*.scss', ['scss']);
 });
+
